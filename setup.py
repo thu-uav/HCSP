cfeb72ba3eaa4569a6773a478550d87aeba2dc84
zhangruize <2290321870@qq.com>
2026-01-03 20:00:26 +0800

    fix: add setup.py
    Signed-off-by: zhangruize <2290321870@qq.com>

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hcsp",
+    version="0.0.0",
+    author="zhangruize",
+    keywords=["robotics", "rl"],
+    packages=find_packages("."),
+    install_requires=[
+        "hydra-core",
+        "omegaconf",
+        "wandb",
+        "moviepy",
+        "imageio",
+        "plotly",
+        "einops",
+        "av",
+        "pandas",
+        "h5py",
+        "filterpy",
+        "usd-core==23.2",
+        "numpy==1.23.5",
+        "urllib3==1.26.18",
+        "seaborn"
+    ],
+)
